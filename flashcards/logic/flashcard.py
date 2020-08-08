07f568b8a368fd75a52770fc204f2313fe8b3172
annbgn <fake@mail.com>
2020-08-08 12:16:41 +0300

    switch to mymemory translation

--- flashcards/logic/flashcard.py
+++ flashcards/logic/flashcard.py
@@ -1,24 +1,37 @@
-from .duo import duo
 import datetime
+
+from translate import Translator
+
+from .duo import duo
 
 
 class Deck:
-    target_lang  = None
-    known_lang = 'en'
+    target_lang = None
+    known_lang = "en"
     flashcards = []
 
     def fill_deck(self, raw, target_lang, known_lang):
         self.target_lang = target_lang
         self.known_lang = known_lang
         self.flashcards = []
+        translator = Translator(
+            provider="mymemory", to_lang=known_lang, from_lang=target_lang
+        )
+        print(target_lang, " ", known_lang)
         for i in raw:
-            self.flashcards.append(Flashcard(i, self.target_lang, self.known_lang))
+            self.flashcards.append(
+                Flashcard(i, self.target_lang, self.known_lang, translator)
+            )
 
     def __str__(self):
-        return "Deck({}, {}, {} flashcards)".format(self.target_lang, self.known_lang, len(self.flashcards))
+        return "Deck({}, {}, {} flashcards)".format(
+            self.target_lang, self.known_lang, len(self.flashcards)
+        )
 
     def __repr__(self):
-        return "Deck({}, {}, {} flashcards)".format(self.target_lang, self.known_lang, len(self.flashcards))
+        return "Deck({}, {}, {} flashcards)".format(
+            self.target_lang, self.known_lang, len(self.flashcards)
+        )
 
     def __iter__(self):
         for fc in self.flashcards:
@@ -29,13 +42,13 @@ class Deck:
 
 
 class Flashcard:
-    def __init__(self, entry, target_lang, known_lang):
+    def __init__(self, entry, target_lang, known_lang, translator):
         self.target_lang = target_lang
         self.target_lang = known_lang
-        self.front = entry.get('word_string')
-        normalized_string = entry.get('normalized_string')
+        self.front = entry.get("word_string")
         start = datetime.datetime.now()
-        self.back = duo.duo.get_translations([normalized_string], source=known_lang, target=target_lang)[normalized_string] or "no data"
+        self.back = translator.translate(self.front)
+        print(self.front, " ", self.back)
         end = datetime.datetime.now() - start
         print(end)
         start = datetime.datetime.now()
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
-import duolingo
-from .logic.duo import duo
-import random
 import datetime
+import random
+
+import duolingo
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+
 import ujson
+
+from .logic.duo import duo
 
 
 def index(request):
@@ -50,8 +53,10 @@ def get_login_data_view(request):
 @csrf_exempt
 def wordlist(request):
     from .logic.flashcard import Deck, deck
+
     lang = request.POST.get("lang", None)
     # skills = list(map(lambda x: x.get('name', '').lowercase(), duo.duo.get_learned_skills(lang)))
+    print('get vocab', end='')
     start = datetime.datetime.now()
     vocab_dict = duo.duo.get_vocabulary(language_abbr=lang)
     end = datetime.datetime.now() - start
@@ -59,11 +64,19 @@ def wordlist(request):
     amount = len(vocab_dict.get("vocab_overview", []))
     # sorted(vocab_dict, key=lambda x: x['order'], reverse=True)
 
-    target_lang = vocab_dict.get('learning_language')
-    known_lang = vocab_dict.get('from_language')
-    flashcard_billets = random.sample(vocab_dict.get("vocab_overview", []), k=20 if amount > 20 else amount)
+    target_lang = vocab_dict.get("learning_language")
+    known_lang = vocab_dict.get("from_language")
+    flashcard_billets = random.sample(
+        vocab_dict.get("vocab_overview", []), k=20 if amount > 20 else amount
+    )
 
+    print('fill deck', end='')
+    start = datetime.datetime.now()
     deck = deck.fill_deck(flashcard_billets, target_lang, known_lang)
+    print(deck)
+    end = datetime.datetime.now() - start
+    print(end)
+    
     context = {"deck": deck}
     return render(request, "flashcards/wordlist.html", context)
 
@@ -71,7 +84,8 @@ def wordlist(request):
 @csrf_exempt
 def practice(request):
     from .logic.flashcard import Deck, deck
+
     data = {fc.front: fc.back for fc in deck.flashcards}
     context = {"deck": ujson.dumps(data)}
-    print (context)
+    print(context)
     return render(request, "flashcards/practice.html", context)
