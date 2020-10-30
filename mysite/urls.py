26482f4b9aef5757f13ce83500051a41ba777f8a
Aditya Rawat <adityainferno4812@gmail.com>
2020-10-30 21:18:15 +0530

    Fix redirect empty path to /flashcards

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('flashcards/', include('flashcards.urls')),
+    path('', include('flashcards.urls')),
     path('admin/', admin.site.urls),
 ]
